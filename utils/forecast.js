91e0293fdabf1fa67d96658321039f9c3dc6fb7e
Manoj Khatri <manojkrk8@gmail.com>
2020-08-21 17:31:32 -0700

    Added Humidity data

--- utils/forecast.js
+++ utils/forecast.js
@@ -9,7 +9,9 @@ const forecast = (latitude, longitude, callback)=>{
         }else if(body.error){
             callback('Unable to find location. Try another search', undefined)
         }else{
-            callback(undefined, body.current.weather_descriptions[0]+ '!! It is currently ' + body.current.temperature + ' degrees and it feels like ' + body.current.feelslike + ' degrees')
+            console.log(body.current)
+            callback(undefined, body.current.weather_descriptions[0]+ '!! It is currently ' + body.current.temperature + ' degrees and it feels like ' + body.current.feelslike + ' degrees'
+            + ' The humidity is ' + body.current.humidity + '%')
         }
     })
 }
